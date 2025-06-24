c39b582e7cf0a4836738069118a17af42f562247
Jerzy Jamroz <jerzy.jamroz@gmail.com>
2025-06-24 11:27:57 +0200

    test: SIGTERM test.

--- mrfApp/src/mrfMain.cpp
+++ mrfApp/src/mrfMain.cpp
@@ -11,13 +11,32 @@
 #include "epicsThread.h"
 #include "iocsh.h"
 
-int main(int argc,char *argv[])
+#include <csignal>
+
+// Signal handler function
+void handle_sigterm(int signum)
 {
-    if(argc>=2) {    
+
+    iocshCmd("dbpf VAN:EVR-1:DCEna-Sel Disable");
+    iocshCmd("dbpf");
+    iocshCmd("date");
+    printf("&&&&&&&&& mrfMain.cpp SIGTERM &&&&&&&&&&&\n");
+    // Optionally, exit the program
+    // exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+    printf("---------------------!!! mrfMain.cpp !!!---------------------\n");
+    // Register the signal handler for SIGTERM
+    std::signal(SIGTERM, handle_sigterm);
+    if (argc >= 2)
+    {
         iocsh(argv[1]);
         epicsThreadSleep(.2);
     }
     iocsh(NULL);
+    printf("=================================!!! mrfMain.cpp EXIT !!!=================================\n");
     epicsExit(0);
-    return(0);
+    return (0);
 }
